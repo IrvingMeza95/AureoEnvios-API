0d67a8b28e5305c7e61565a97496306055b82f01
Irving Meza <irvingmeza95@gmail.com>
2024-07-14 18:30:19 -0500

    se modifico el script .sh

---
+++ cloud_scripts/README.txt.txt
@@ -0,0 +1,2 @@
+#Script para otorgar permisos al archivo sh
+chmod +x /root/DonWeb/AureoEnvios-API/cloud_scripts/run_bat.sh
--- cloud_scripts/run_bat.sh
+++ cloud_scripts/run_bat.sh
@@ -1,1 +1,26 @@
-wine cmd /c /root/DonWeb/AureoEnvios-API/cloud_scripts/Respaldo_local_db.bat
+#!/bin/bash
+
+user="root"
+pw="sasa"
+host="localhost"
+port="3307"
+
+# Obtener fecha y hora
+anio=$(date +%Y)
+mes=$(date +%m)
+dia=$(date +%d)
+hora=$(date +%H)
+minutos=$(date +%M)
+segundos=$(date +%S)
+
+# Crear nombre de archivo
+nombre="aureo_envios_db_local_${anio}${mes}${dia}_${hora}${minutos}${segundos}.sql"
+path="/root/DonWeb/Respaldos_db/${nombre}"
+
+# Ejecutar mysqldump y manejar errores
+echo "Haciendo respaldo de la base de datos..."
+if mysqldump -h $host --port=$port -u $user -p$pw aureo_envios_db > $path; then
+    echo "Respaldo completado exitosamente."
+else
+    echo "Error: Fallo el respaldo de la base de datos."
+fi
